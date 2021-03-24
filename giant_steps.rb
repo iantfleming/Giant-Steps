a52d808a8792a718c12718b51e92d171bb212dc2
iantfleming <iantfleming1992@gmail.com>
2021-03-24 20:24:00 +0000

    giant steps

---
+++ giant_steps.rb
@@ -0,0 +1,316 @@
+define :giant_steps_melody do
+  in_thread do
+    2.times do
+      use_synth :piano
+      note_play 78, 1
+      rest 0.5
+      note_play 74, 1
+      rest 0.5
+      note_play 71, 1
+      rest 0.5
+      note_play 67, 1
+      rest 0.375
+      note_play 70, 1
+
+      rest 1
+
+      note_play 71, 1
+      rest 0.5
+      note_play 69, 1
+
+      rest 0.5
+
+      note_play 74, 1
+      rest 0.5
+      note_play 70, 1
+      rest 0.5
+      note_play 67, 1
+      rest 0.5
+      note_play 63, 1
+      rest 0.375
+      note_play 66, 1
+
+      rest 1
+
+      note_play 67, 1
+      rest 0.5
+      note_play 65, 0.45
+      rest 0.375
+      note_play 70, 0.5
+
+      rest 1
+
+      note_play 71, 1
+      rest 0.5
+      note_play 69, 1
+      rest 0.375
+      note_play 74, 1
+
+      rest 1
+
+      note_play 75, 1
+      rest 0.5
+      note_play 73, 1
+      rest 0.375
+      note_play 78, 1
+
+      rest 1
+
+      note_play 79, 1
+      rest 0.5
+      note_play 77, 1
+      rest 0.375
+      note_play 82, 1
+
+      rest 1
+
+      note_play 78, 1
+      rest 0.375
+      note_play 78, 0.5
+
+      rest 0.5
+    end
+  end
+end
+
+define :giant_steps_bass do
+  in_thread do
+    2.times do
+      use_synth :piano
+      note_play 47, 1
+      rest 0.5
+      note_play 50, 1
+      rest 0.5
+      note_play 43, 1
+      rest 0.5
+      note_play 46, 1
+      rest 0.375
+      note_play 39, 1
+
+      rest 1
+
+      note_play 45, 1
+      rest 0.5
+      note_play 38, 1
+
+      rest 0.5
+
+      note_play 43, 1
+      rest 0.5
+      note_play 46, 1
+      rest 0.5
+      note_play 39, 1
+      rest 0.5
+      note_play 42, 1
+      rest 0.375
+      note_play 35, 1
+      rest 0.5
+      note_play 42, 0.5
+      rest 0.25
+      note_play 42, 0.5
+      rest 0.25
+      note_play 34, 0.5
+
+
+
+      note_play 41, 1
+      rest 0.5
+      note_play 46, 1
+      rest 0.375
+      note_play 39, 0.5
+      rest 0.5
+      note_play 34, 0.5
+
+      rest 0.5
+
+      note_play 45, 1
+      rest 0.5
+      note_play 38, 1
+      rest 0.375
+      note_play 43, 1
+
+      rest 1
+
+      note_play 49, 1
+      rest 0.5
+      note_play 42, 1
+      rest 0.375
+      note_play 47, 1.125
+      note_play 42, 0.5
+      note_play 35, 0.5
+
+      rest 1
+
+      note_play 41, 1
+      rest 0.5
+      note_play 46, 1
+      rest 0.375
+      note_play 39, 1
+
+      rest 1
+
+      note_play 37, 1
+      rest 0.375
+      note_play 42, 0.5
+
+      rest 0.5
+    end
+  end
+end
+
+define :giant_steps_harmony do
+  in_thread do
+    2.times do
+      use_synth :piano
+      note_play 75, 1
+      note_play 70, 1
+      note_play 66, 1
+      note_play 58, 1
+      rest 0.5
+      note_play 71, 1
+      note_play 66, 1
+      note_play 64, 1
+      note_play 60, 1
+      note_play 54, 1
+      rest 0.5
+      note_play 66, 1
+      note_play 62, 1
+      note_play 52, 1
+      note_play 50, 1
+      rest 0.5
+      note_play 62, 1
+      note_play 56, 1
+      rest 0.375
+      note_play 63, 1
+      note_play 62, 1
+      note_play 55, 1
+
+      rest 1
+
+      note_play 67, 1
+      note_play 64, 1
+      note_play 60, 1
+      note_play 55, 1
+      rest 0.5
+      note_play 66, 1
+      note_play 64, 1
+      note_play 60, 1
+      note_play 50, 1
+
+      rest 0.5
+      note_play 71, 1
+      note_play 66, 1
+      note_play 62, 1
+      note_play 52, 1
+      note_play 50, 1
+      rest 0.5
+      note_play 67, 1
+      note_play 62, 1
+      note_play 56, 1
+      rest 0.5
+      note_play 62, 1
+      note_play 55, 1
+      note_play 46, 1
+      rest 0.5
+      note_play 58, 1
+      note_play 56, 1
+      note_play 52, 1
+      rest 0.375
+      note_play 63, 1
+      note_play 61, 1
+      note_play 58, 1
+
+      rest 1
+
+      note_play 63, 1
+      note_play 60, 1
+      note_play 56, 1
+      rest 0.5
+      note_play 62, 1
+      note_play 60, 1
+      note_play 56, 1
+      rest 0.375
+      note_play 63, 1
+      note_play 62, 1
+      note_play 55, 1
+
+      rest 1
+
+      note_play 64, 1
+      note_play 64, 1
+      note_play 60, 1
+      note_play 55, 1
+      rest 0.5
+      note_play 66, 1
+      note_play 64, 1
+      note_play 60, 1
+      rest 0.375
+      note_play 74, 1
+      note_play 66, 1
+      note_play 62, 1
+      note_play 59, 1
+      note_play 50, 1
+
+      rest 1
+
+      note_play 71, 1
+      note_play 68, 1
+      note_play 64, 1
+      note_play 59, 1
+      rest 0.5
+      note_play 70, 1
+      note_play 67, 1
+      note_play 64, 1
+      rest 0.375
+      note_play 73, 1
+      note_play 70, 1
+      note_play 68, 1
+      note_play 58, 1
+
+      rest 1
+
+      note_play 75, 1
+      note_play 72, 1
+      note_play 68, 1
+      note_play 56, 1
+      rest 0.5
+      note_play 74, 1
+      note_play 72, 1
+      note_play 68, 1
+      note_play 56, 1
+      rest 0.375
+      note_play 79, 1
+      note_play 75, 1
+      note_play 74, 1
+      note_play 55, 1
+
+      rest 1
+      note_play 75, 1
+      note_play 71, 1
+      note_play 68, 1
+      note_play 59, 1
+      rest 0.375
+      note_play 75, 1
+      note_play 70, 1
+      note_play 68, 1
+      note_play 58, 1
+
+      rest 0.5
+
+    end
+  end
+end
+
+define :note_play do |num, note_length|
+  in_thread do
+    play num, release: note_length
+  end
+end
+
+define :rest do |before_start|
+  sleep before_start
+end
+
+giant_steps_melody
+giant_steps_bass
+giant_steps_harmony
